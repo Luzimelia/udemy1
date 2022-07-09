97aae5587ca5f07c698dc9d745afa25ce73ddcbd
Luzimélia <103471507+Luzimelia@users.noreply.github.com>
2022-07-09 13:41:42 -0300

    Add files via upload

---
+++ exercicios_js/usandoVar.js
@@ -0,0 +1,23 @@
+// Escopo - local onde aquela variável é visível
+
+{
+       {
+         {
+              {
+    var sera = 'Será???'
+    console.log(sera)
+              }
+         }
+      }
+}
+    
+    console.log(sera)
+    
+    function teste () {
+    var local = 123
+    console.log (local)
+    }
+    
+    teste()
+    console.log (local)
+    
