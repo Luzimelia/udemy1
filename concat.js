664c760d3cb36781cf9f1e500e68d0517158f18d
Luzimélia <103471507+Luzimelia@users.noreply.github.com>
2022-08-10 10:22:25 -0300

    Add files via upload

---
+++ concat.js
@@ -0,0 +1,6 @@
+const filhas = ['Valeskah', 'Cibalena']
+const filhos = ['Uoxito', 'Uesclei']
+const totos = filhas.concat(filhos, 'Fulano')
+console.log(todos, filhas, filhos)
+
+console.log([].concat([1,2], [3,4], [[6,7]]))
